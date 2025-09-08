b434ae2a8a0407e6fbd508ddefe365ea5f1eab71
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-09-09 02:26:16 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,35 @@
+package printlnprintprintf;
+
+public class PrintlnPrintPrintf {
+
+    public static void main(String[] args) {
+        //tipe data yang digunakan dan nama variabel//
+   String nama;
+   int umur;
+   double tb;
+   
+   //Deklarasi//
+    nama = "mohammad rasya romadhan";
+    umur = 19;
+    tb = 168.0;
+    
+    //Untuk penggunaan "println" mencetak agar otomatis pindah dibaris baru//
+        System.out.println("ig_mhmdrsyaaa_");
+        
+    //untuk penggunaan "print" mencetak teks agar tanpa pindah baris//
+        System.out.print("perkenalkan");
+        
+    /*Untuk penggunaan "printf" mencetak teks dengan format
+        %s = String
+        %d = byte, short, int, long
+        %.2f = float dan double 
+        (untuk menampilkan 2 angka dibelakang koma pakai " %.2.f "
+         umtuk menampilkan banyak angka dibelakang koma pakai " %f " */
+        System.out.printf(" nama saya : %s ,umur : %dtahun ,Tinggi Badan : %.2f ",nama, umur, tb);
+        //nama saya : mohammad rasya romadhan ,umur : 19tahun ,Tinggi Badan : 168,00//
+   
+        
+        
+    }
+    
+}
