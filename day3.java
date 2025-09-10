6c5f9fc1bd29ff284c832ce01f4f8f9d98830636
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-09-10 19:38:06 +0800

    Create day3.java

---
+++ day3.java
@@ -0,0 +1,19 @@
+package pengunaankomentar;
+
+public class PengunaanKomentar {
+
+    public static void main(String[] args) {
+        
+       // ada dua jenis komentar dalam bahasa java yang saya ketahui:(//) dan (/*)
+       
+       // untuk satu baris saja
+       /* untuk komentar yang lebih dari satu baris
+       1
+       2
+       3
+       dan seterusnya
+       */
+        
+    }
+    
+}
