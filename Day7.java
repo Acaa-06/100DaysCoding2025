776f9c0eca1ebefa8c3c4b66ee482582973bf4a9
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-09-14 19:20:25 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,21 @@
+package charbool;
+
+public class CharBool {
+
+
+    public static void main(String[] args) {
+ 
+     // boolean untuk menentukan true/false atau benar salah 
+     boolean aca1 = true;
+     boolean aca2 = false;
+     
+     // char digunakan untuk penggunaan satu karakter misalnya (A),(6),(@)
+     char gender = '@';
+     
+        System.out.println("Apa benar nama saya (aca) = " + true);
+        System.out.println("Apa kamu gamon = " +false);
+        System.out.println(gender +"_mhmdrsyaaa_");
+
+    }
+    
+}
