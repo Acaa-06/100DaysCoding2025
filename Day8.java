159338b435f6446206c696da7fc47d5b6b10b934
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-09-15 19:25:48 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,12 @@
+package stringaca;
+
+public class StringAca {
+
+    public static void main(String[] args) {
+       //String Tipe DAT untuk teks dan penggunaan String menggunakan dua kutip (")
+    
+       String Teks = "Acaaaaaaaaaaa";
+        System.out.println("Nama Panggilan \t: " + Teks);
+    }
+    
+}
