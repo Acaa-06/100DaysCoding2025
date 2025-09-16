934370a1008c48465237fa85eb9841a29edd0c25
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-09-16 14:08:37 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,17 @@
+package acaday9;
+
+public class AcaDay9 {
+
+    public static void main(String[] args) {
+       int HargaAwal,HargaUpdate,HargaAkhir;
+       HargaAwal = 100;
+       HargaUpdate = 20;
+       HargaAkhir = HargaAwal + HargaUpdate;
+       
+        System.out.println("Harga Awal \t= " +HargaAwal);
+        System.out.println("Harga Update \t= " +HargaUpdate);
+        System.out.println("Harga Akhir \t= " +HargaAkhir);
+       
+    }
+    
+}
