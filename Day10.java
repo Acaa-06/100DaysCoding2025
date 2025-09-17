4ab1081f8cbf4dedbd87142af3edc9ba8525ec8d
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-09-17 21:14:19 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,14 @@
+package constanta;
+
+public class Constanta {
+
+    public static void main(String[] args) {
+        int Aca1 = 150;
+        final int Aca2 = 120;
+     //Constanta ini adalah hasil akhir atau hasil yang sudah tidak diubah 
+     
+        System.out.println("Harga Normal \t= " +Aca1);
+        System.out.println("Harga Net \t= " +Aca2);
+    }
+    
+}
