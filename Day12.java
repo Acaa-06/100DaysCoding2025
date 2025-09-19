9277556de8c52496de4e84d4e9f9809d835ea08e
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-09-19 13:49:59 +0800

    Create Day12.java

---
+++ Day12.java
@@ -0,0 +1,33 @@
+package day12aca;
+
+public class Day12Aca {
+
+    public static void main(String[] args) {
+      String aca1,aca2,aca3,aca4,aca5,aca8,aca9,aca10;
+      int aca6,aca7;
+      
+      aca1 = "Mohammad Rasya Romadhan";
+      aca2 = "Lembang Dhua";
+      aca3 = "Majene";
+      aca4 = "Mengagumi";
+      aca5 = "089517107477";
+      aca6 = 06;
+      aca7 = 2006;
+      aca8 = "Single Era";
+      aca9 = "UPTD SMKN 2 Majene";
+      aca10 = "@_mhmdrsyaaaa_";
+      
+      
+        System.out.println("BIO DATA");
+        System.out.println("Nama \t\t: "+aca1);
+        System.out.println("Alumni \t\t: "+aca9);
+        System.out.println("Alamat \t\t: "+aca2);
+        System.out.println("TTL \t\t: "+aca3+"\\"+aca6 +" October "+ aca7);
+        System.out.println("Hoby \t\t: "+aca4);
+        System.out.println("Status \t\t: "+aca8);
+        System.out.println("No HP \t\t: "+aca5);
+        System.out.println("Instagram \t: "+aca10);
+        
+    }
+    
+}
