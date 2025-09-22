e78d59f7302bdced12ff341f40cddc873d998303
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-09-22 21:09:58 +0800

    Create Day15.java
    penjumlahan dan pengurangan aritmatika

---
+++ Day15.java
@@ -0,0 +1,16 @@
+package tambahdankurang;
+public class TambahDanKurang {
+
+    public static void main(String[] args) {
+       
+      int a,b,c;
+       a = 6;
+       b = 10;
+       
+       c = a + b;
+        System.out.println(a+" + "+b+" = "+c);
+       c = b - a;
+        System.out.println(b+" - "+a+" = "+c);
+    }
+    
+}
