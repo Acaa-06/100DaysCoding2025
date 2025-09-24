d77487cc70abf302ed186bc2f4342052cccf8d5e
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-09-24 14:40:50 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,22 @@
+package modolus;
+import java.util.Scanner;
+
+public class Modolus {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        double a,b,c;
+        System.out.print("Masukkan a = ");
+        a =input.nextDouble();
+        
+        System.out.print("Masukkan b = ");
+        b = input.nextDouble();
+        
+        c = a % b;
+        System.out.println(a+" : "+b+" = "+c);
+        
+        
+    }
+    
+}
