9e43ba8eaf9b676c9ad939cd10dd63579e64580a
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-09-26 22:34:28 +0800

    Create Day19.java
    Konversi Paksa

---
+++ Day19.java
@@ -0,0 +1,23 @@
+package konversipaksa;
+
+public class KonversiPaksa {
+
+    public static void main(String[] args) {
+        //KONVERSI PAKSA
+        
+        long a = 69;
+        int b = (int)a;
+        short c = (short)b;
+        byte d = (byte)c;
+        double e = (double)d;
+        float f = (float)e;
+        
+        
+        System.out.println("long ke int \t= "+b);
+        System.out.println("int ke short \t= "+c);
+        System.out.println("short ke byte \t= "+d);
+        System.out.println("byte ke double \t= "+e);
+        System.out.println("double ke float = "+f);
+                   }
+                
+                }
