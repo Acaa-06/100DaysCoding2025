a52b8bab8c31b14374a24607a90add6236426dfe
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-10-03 23:15:49 +0800

    Create Day26.java
    penugasan

---
+++ Day26.java
@@ -0,0 +1,23 @@
+package day26;
+public class Day26 {
+
+    public static void main(String[] args) {
+       
+        int a = 10; 
+        int b = 6;
+        
+        a += b;
+        System.out.println(a);//16
+        a -= b;
+        System.out.println(a);//10
+        a /= b;
+        System.out.println(a);//1
+        a *= b;
+        System.out.println(a);//6
+        a %= b;
+        System.out.println(a);//0
+        
+        
+    }
+    
+}
