01e4dadff305db97c537a4fd4ed6d2119df8e043
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-10-05 03:48:34 +0800

    Create Day28.java
    perbandingan

---
+++ Day28.java
@@ -0,0 +1,12 @@
+package rasyy;
+
+public class Day28 {
+    public static void main(String[] args) {
+        String a = "cowo";
+        String b = "cewe";
+        
+        System.out.println(a == b);
+        System.out.println(b != a);
+                
+}
+}
