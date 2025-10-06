d95889e7534189b8633693ce840776f11aa9ce4d
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-10-06 22:51:23 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,12 @@
+package rasyy;
+public class Day29 {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 5;
+
+        System.out.println(a > b); // true
+        System.out.println(a < b); // false
+        
+    }
+}
