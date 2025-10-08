083af8fd03f90db68b0f7ab3e116ddf43681e911
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-10-08 23:39:37 +0800

    Create Day31.java

---
+++ Day31.java
@@ -0,0 +1,13 @@
+public class Aca{
+    public static void main(String[] args) {
+        int umur = 20;
+       boolean besar = true;
+      
+        System.out.println("apakah saya sudah besar: " + (umur <30 && besar));   // true
+   
+
+      
+    }
+}
+
+    
