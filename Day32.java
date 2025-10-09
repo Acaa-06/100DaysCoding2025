cbc21d31dc2767e49a21ba90a67755abdd03c7db
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-10-09 22:58:33 +0800

    Create Day32.java

---
+++ Day32.java
@@ -0,0 +1,11 @@
+public class OperatorOR {
+    public static void main(String[] args) {
+        
+        int aku = 20;
+        int dia = 26;
+
+        boolean bersama = (aku >= 21 || dia >= 25);
+
+        System.out.println("Apakah dia lebih tua? " + bersama);
+    }
+}
