46301bbbb06b2c490914930b595f0a786a1cd2d3
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-10-14 20:42:14 +0800

    Create Day37.java

---
+++ Day37.java
@@ -0,0 +1,24 @@
+package Rasyyy;
+
+import java.util.Scanner;
+
+public class Acaa {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Masukkan a : ");
+        int a = in.nextInt();
+
+        if ( a % 2 ==0 ) {
+           System.out.println(a + " Genap")
+        }else {
+           System.out.println(a + " Ganjil")
+        }
+
+    
+
+    }
+
+}
