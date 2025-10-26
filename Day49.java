97fbc5931e2a2a1dc5c113a9b1602315d8fcc143
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-10-26 23:49:26 +0800

    Create Day49.java

---
+++ Day49.java
@@ -0,0 +1,18 @@
+package Rasyy;
+
+import java.util.Scanner;
+
+public class Ternary {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Masukkan umur: ");
+        int umur = in.nextInt();
+
+        String status = (umur >= 18) ? "Dewasa" : "Belum Dewasa";
+        System.out.println("Status: " + status);
+       
+    }
+
+}
