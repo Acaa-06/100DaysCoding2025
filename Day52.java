8a8d0272583ac9b194950207c6a24033c641cf14
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-10-29 23:42:20 +0800

    Create Day52.java
    while

---
+++ Day52.java
@@ -0,0 +1,15 @@
+package rasyy;
+
+
+public class ForWhile {
+
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 10) {
+        System.out.printf("%d ",i );
+            i++;        // menaikkan nilai i
+        }
+        System.out.println("");
+    }
+}
