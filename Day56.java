7966b09d83a07c58c994bb33cb3f6f225e250c55
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-11-02 23:19:21 +0800

    Create Day56.java

---
+++ Day56.java
@@ -0,0 +1,15 @@
+package rasyy;
+
+public class Acaaa {
+
+    public static void main(String[] args) {
+
+        int a = 1;
+
+        do {
+            System.out.println("Angka ke-" + a);
+            a++;
+        } while (a <= 10);
+
+    }
+}
