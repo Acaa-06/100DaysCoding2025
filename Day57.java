0ea79b4cc982f47c1ffa499d8b29eabe219264aa
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-11-03 23:30:40 +0800

    Create Day57.java

---
+++ Day57.java
@@ -0,0 +1,16 @@
+package rasyy;
+
+public class Acaa {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Loop i : " + i);
+            for (int j = 1; j <= 2; j++) {
+                System.out.println("  Loop j : " + j);
+            }
+        }
+
+    }
+
+}
