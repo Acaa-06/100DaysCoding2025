c38d760ce5e0a63faa94a0c799cd57c2af52e0dd
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-11-13 22:48:06 +0800

    Create Day67.java

---
+++ Day67.java
@@ -0,0 +1,14 @@
+package rasyy;
+
+public class PolaHorizontal {
+
+    public static void main(String[] args) {
+
+    System.out.println("Pola Horizontal");
+        for (int i = 1; i <= 6; i++) {
+               System.out.print("**");
+        }
+    }
+}
+      
+        
