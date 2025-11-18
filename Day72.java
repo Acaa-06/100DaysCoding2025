60669d37e5d79d9f2361c78a3cad7e1ca050e548
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-11-18 23:25:52 +0800

    Create Day72.java

---
+++ Day72.java
@@ -0,0 +1,17 @@
+package rasyy;
+
+import java.util.Scanner;
+
+public class ForWhile {
+
+    public static void main(String[] args) {
+        
+        for (int i = 5; i >0; i--) {
+            for (int j = i; j >0 ; j--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        
+    }
+}
