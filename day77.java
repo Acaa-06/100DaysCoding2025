ce83ae096f8b22a5daaa02bee75df73299ac3e1a
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-11-23 21:21:33 +0800

    Create day77.java

---
+++ day77.java
@@ -0,0 +1,19 @@
+package rasyy;
+
+// import java.util.Scanner;
+
+public class ForWhile {
+
+    public static void main(String[] args) {
+        
+        String sub =" anjayyy suranjay ";
+        
+        System.out.println("Substring : "+sub.substring(12,17));
+        System.out.println("Replace   : "+sub.replace("suranjay", "acaa"));
+        System.out.println("Trim      : "+sub.trim());
+        
+
+
+
+    }
+}
