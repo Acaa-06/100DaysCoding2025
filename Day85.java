ddb00ae47db39de453af3f3eef5a12664c670d8e
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-12-01 23:37:38 +0800

    Create Day85.java

---
+++ Day85.java
@@ -0,0 +1,15 @@
+package rasyy;
+
+public class Acaa {
+    public static void main(String[] args) {
+        int [] angka = {10,30,20,100,80,50};
+        int max = angka[0];
+      
+        for (int i = 0; i < angka.length; i++) {
+            if (angka[i] > max) {
+                max = angka[i];
+            }
+        }
+        System.out.println("Nilai maxsimal : " + max);
+    }
+}
