19894716e96f06407423b9093947e8f69998af12
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-12-02 23:41:13 +0800

    Create Day86.java

---
+++ Day86.java
@@ -0,0 +1,13 @@
+package rasy;
+public class aca {
+    public static void main(String[] args) {
+        int [] angkaArray = {10,30,20,100,80,50};
+        int min = angkaArray[0];
+        for (int i = 0; i < angkaArray.length; i++) {
+            if (angkaArray[i] < min) {
+                min = angkaArray[i];
+            }
+        }
+        System.out.println("Nilai minimal : " + min);
+    }
+}
