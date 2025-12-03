c131cb0e4bfb18f668cefbb7917bdf110955c227
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-12-03 22:49:30 +0800

    Create Day87.java

---
+++ Day87.java
@@ -0,0 +1,26 @@
+package rasy;
+
+import java.util.Scanner;
+
+public class  Acaa{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int angka [] = {10,50,100,80,30};
+        System.out.print("Mencari angka : ");
+        int cari = in.nextInt();
+        
+        boolean ditemukan = false;
+        for (int a : angka) {
+            if (a == cari) {
+                ditemukan = true;
+            }
+        }
+      
+        if (ditemukan) {
+                System.out.println("angka "+ cari + " ditemukan");    
+        }else{
+                System.out.println("angka "+ cari + " tidak ditemukan");    
+        }
+      
+    }
+}
