5edfc4c1145c05e8606dec34d9315d774816c045
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-12-04 23:32:03 +0800

    Create Day88.java

---
+++ Day88.java
@@ -0,0 +1,10 @@
+package rasy;
+public class Acaa {
+    public static void main(String[] args) {   
+        char[] array = {'r','a','s','y','a'}; 
+        for (char b : array ) {
+            System.out.print(b+ " ");
+        }  
+        System.out.println();
+    }
+}
