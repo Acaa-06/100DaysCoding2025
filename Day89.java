2beb7fd866856a16b52972a484d55eb24ab6911e
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-12-05 23:45:55 +0800

    Create Day89.java

---
+++ Day89.java
@@ -0,0 +1,9 @@
+package rasyy;
+public class Acaa {
+    public static void teks() {
+        System.out.println("Anjayyy");
+    }
+    public static void main(String[] args) {
+        teks();
+    }
+}
