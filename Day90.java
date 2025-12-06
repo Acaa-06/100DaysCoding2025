be79cf20fa0524aa59b450f7dfeca9e61152af89
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-12-06 23:52:44 +0800

    Create Day90.java

---
+++ Day90.java
@@ -0,0 +1,13 @@
+package rasyy;
+
+import java.util.Scanner;
+
+public class Acaa {
+    public static String kata() {
+        String teks = "Anjayyyyy";
+        return teks;
+    }
+    public static void main(String[] args) {
+        System.out.println(kata());
+    }
+}
