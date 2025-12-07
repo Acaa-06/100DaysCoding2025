b1a1e0b1087e56ddbf1db4507c05fd1f8a1efeed
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-12-07 23:14:11 +0800

    Create Day91.java

---
+++ Day91.java
@@ -0,0 +1,9 @@
+public class day91 {
+    public static void a(String anjay) {
+        System.out.print(anjay);
+    }  
+        public static void main(String[] args) {
+        a("anjay");
+          
+        }
+}
