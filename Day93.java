ac7b699c50ea07788327f34af4b3944192e243e7
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-12-09 23:49:19 +0800

    Create Day93.java

---
+++ Day93.java
@@ -0,0 +1,13 @@
+package rasyy;
+public class Acaa {
+    public static void main(String[] args) {
+        double angka1 = 14.0108;
+        double angka2 = 55.4;
+        double angka3 = 10.0920;
+        
+        System.out.println(Math.ceil(angka1));
+        System.out.println(Math.floor(angka2));
+        System.out.println(Math.round(angka3));
+    }
+    
+}
