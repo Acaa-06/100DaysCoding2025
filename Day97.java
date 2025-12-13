a0651a8a855d03dc740f6b2693b3819334f3e1bc
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-12-13 23:12:39 +0800

    Create Day97.java

---
+++ Day97.java
@@ -0,0 +1,22 @@
+package rasyy;
+
+import java.util.Scanner;
+
+public class Acaa {
+  public static int hitungLuas(int s) {
+    int Luas = s * s;
+    System.out.println("Luas Persegi = " + Luas);
+    return Luas;
+  }
+  public static void main(String[] args) {
+    Scanner in = new Scanner (System.in);
+
+    System.out.println("Masukkan Sisi : ");
+    int a = in.nextInt();
+
+    hitungLuas(a);
+
+  }
+}
+      
+      
