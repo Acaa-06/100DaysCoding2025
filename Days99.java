58dcbd5ee9f91f17d53ceefd3756011f59858256
Acaa-06 <rasyaaromadhan6106@gmail.com>
2025-12-15 23:35:29 +0800

    Create Days99.java

---
+++ Days99.java
@@ -0,0 +1,30 @@
+package rasyy;
+
+import java.util.Scanner;
+
+public class Acaa {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Masukkan Batasan : ");
+        int a = in.nextInt();
+        
+        System.out.print("========================\n");
+        System.out.print("Deretan bilangan prima: ");
+
+        for (int i = 2; i <=a; i++) {
+            int pembagi = 0;
+
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    pembagi++;
+                }
+            }
+
+            if (pembagi == 2) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println("");
+    }
+}
